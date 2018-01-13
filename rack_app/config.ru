705e95cc5119f87ada9baf127a0b6606869213d2
Lorenzo Grandi <lorenzo@wetransfer.com>
2018-01-13 10:50:41 +0100

    Use different config in config.ru

---
+++ rack_app/config.ru
@@ -0,0 +1,3 @@
+require_relative 'fortune'
+
+run Fortune.http
--- rack_app/fortune.rb
+++ rack_app/fortune.rb
@@ -4,4 +4,10 @@ class Fortune
   def self.wisdom
     `fortune`
   end
+
+  def self.http
+    Proc.new do |env|
+      ['200', { 'Content-Type' => 'text/plain' }, [Fortune.wisdom]]
+    end
+  end
 end
